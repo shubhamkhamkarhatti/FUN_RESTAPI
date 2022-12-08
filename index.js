a2dcf46ab09949c7b92b73c61410784896b9d96f
shubham <2gi20mc069@students.git.edu>
2022-12-08 20:12:23 +0530

    Error handling of route created

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require("express")
+const morgan = require("morgan")
+const createError = require("http-errors")
+require("dotenv").config()
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.get('/funApi',(req, res)=>{
+app.use(morgan('dev'))
+
+app.get('/funapi',(req, res)=>{
     res.send("First message from API..")
 })
 
 app.get('/',(req, res)=>{
     res.send("Main Api of the FUNAPI..")
 })
+
+// Error handling 
+app.use((req, res, next)=>{
+    // const error = new Error('Not Found')
+    // error.status = 404
+    // next(error) 
+    next(createError.NotFound())
+})
+app.use((err, req, res, next)=>{
+    res.status(err.status || 500)
+    res.send({
+        "error":{
+            "status":err.status || 500,
+            "message":err.message
+        }
+    })
+})
+
 
 app.listen(port,()=>console.log(`Server up and running on port ${port}..`))
